158339d185353d4e96944219a17742eb5b62b309
BigPolarBear <hanxincade@gmail.com>
2013-10-10 14:10:52 +0800

    reinit repo

---
+++ iOS7TestTests/iOS7TestTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS7TestTests.m
+//  iOS7TestTests
+//
+//  Created by BigPolarBear on 13-10-8.
+//  Copyright (c) 2013年 bigpolarbear.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS7TestTests : XCTestCase
+
+@end
+
+@implementation iOS7TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
